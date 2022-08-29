a3e2aeb951fbbf1e80ba62db607b1c4e2fb33949
Asteises <p.nikitaev@gmail.com>
2022-08-29 23:06:37 +0300

    add new models: Taco, Ingredient, TacoOrder add new controller: DesignTacoController

---
+++ src/main/java/ru/asteises/tacocloud/model/Taco.java
@@ -0,0 +1,13 @@
+package ru.asteises.tacocloud.model;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+
+    private String name;
+    private List<Ingredient> ingredients;
+
+}
