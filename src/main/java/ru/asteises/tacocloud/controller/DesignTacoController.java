90f5ed7e7dce276d8b6fdb6ea932017bccd90cec
Asteises <p.nikitaev@gmail.com>
2022-08-30 17:48:06 +0300

    add new controller: DesignTacoController add new Template pages add new converter: IngredientByIdConverter

--- src/main/java/ru/asteises/tacocloud/controller/DesignTacoController.java
+++ src/main/java/ru/asteises/tacocloud/controller/DesignTacoController.java
@@ -3,10 +3,7 @@ package ru.asteises.tacocloud.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import ru.asteises.tacocloud.model.Ingredient;
 import ru.asteises.tacocloud.model.Taco;
 import ru.asteises.tacocloud.model.TacoOrder;
@@ -53,6 +50,15 @@ public class DesignTacoController {
     public String showDesignForm() {
         return "design";
     }
+
+    @PostMapping
+    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+        tacoOrder.addTaco(taco);
+        log.info("Processing taco: {}", taco);
+
+        return "redirect:/orders/current";
+    }
+
     private Iterable<Ingredient> filterByType(
             List<Ingredient> ingredients, Ingredient.Type type) {
         return ingredients
--- src/main/java/ru/asteises/tacocloud/model/TacoOrder.java
+++ src/main/java/ru/asteises/tacocloud/model/TacoOrder.java
@@ -1,20 +1,32 @@
 package ru.asteises.tacocloud.model;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.CreditCardNumber;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class TacoOrder {
 
+    @NotBlank
     private String deliveryName;
+    @NotBlank
     private String deliveryStreet;
+    @NotBlank
     private String deliveryCity;
+    @NotBlank
     private String deliveryState;
+    @NotBlank
     private String deliveryZip;
+    @CreditCardNumber
     private String ccNumber;
+    @Pattern(regexp = "^(0[1-9]|[0-2])\/?([2-9][0-9]))$")
     private String ccExpiration;
+    @Digits(integer = 3, fraction = 0)
     private String ccCVV;
     private List<Taco> tacos = new ArrayList<>();
     public void addTaco(Taco taco) {
